c219beb39330061c2812e575ee8dcb3e2d416f21
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2025-07-13 18:31:16 +0300

    adjust player size and speed

--- player/player.gd
+++ player/player.gd
@@ -2,11 +2,11 @@ extends CharacterBody2D
 class_name Player
 
 @export_category("Properties")
-@export_range(100.0, 1000.0) var speed: float = 10.0
+@export_range(100.0, 1000.0) var speed: float = 100.0
 
 
 func _physics_process(_delta: float) -> void:
-    var direction: Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
-    velocity = direction * speed
+	var direction: Vector2 = Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down")
+	velocity = direction * speed
 
-    move_and_slide()
+	move_and_slide()
