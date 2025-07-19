f4964a47b47447ae633311835c67dd91997d3da9
AJaxx <62261949+AJaxx86@users.noreply.github.com>
2025-07-19 06:42:31 +0100

    basic ass ground gen

---
+++ generation/ground_gen.gd
@@ -0,0 +1,21 @@
+extends TileMapLayer
+
+@export var region_size: Rect2i = Rect2i(-50, -50, 100, 100)
+
+
+func _ready() -> void:
+	_generate_ground(region_size)
+
+
+func _generate_ground(area: Rect2i) -> void:
+	var terrain_set_id = 0
+	var sand_id = 0
+	var cells: Array[Vector2i] = []
+
+	for x in range(area.position.x, area.position.x + area.size.x):
+		for y in range(area.position.y, area.position.y + area.size.y):
+			cells.append(Vector2i(x, y))
+
+	# Fill them with sand, connecting edges
+	set_cells_terrain_connect(cells, terrain_set_id, sand_id)
+	print_debug("Generated %s cells" % cells.size())
